10a1c2a4a10f0c9f7e7148e0a5159e1999650df4
Dejan Fajfar <dejan@fajfar.com>
2019-06-28 00:09:35 +0200

    Fixed the template loading error

--- src/template.js
+++ src/template.js
@@ -1,17 +1,16 @@
 "use strict";
 
-const fs = require('fs');
 const handlebars = require('handlebars');
 
 const templateFileLocation = './src/template.handlebars';
 
 module.exports.renderTemplate = (options) => {
-	const rawTemplate = fs.readFileSync(templateFileLocation, {
-		encoding: 'utf8',
-		flag: 'r'
-	});
 
-	const compiledTemplate = handlebars.compile(rawTemplate);
+	const compiledTemplate = handlebars.compile(template);
 
 	return compiledTemplate(options);
 };
+
+const template =
+`#!/bin/bash
+`;
